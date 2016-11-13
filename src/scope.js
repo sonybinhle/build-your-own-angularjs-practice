60ee1a0dfd51738bd6a5683e8da300ccc25cfaad
Le Do Thai Binh <xeasony@gmail.com>
2016-11-13 17:34:21 +0100

    Chapter2: implement $evalAsyn --> call function in the near future by setting phase in Angular

--- src/scope.js
+++ src/scope.js
@@ -5,6 +5,8 @@ var _ = require('lodash');
 function Scope() {
     this.$$watchers = [];
     this.$$lastDirtyWatch = null;
+    this.$$asyncQueue = [];
+    this.$$phase = null;
 }
 
 function initWatchVal() {
@@ -65,13 +67,19 @@ Scope.prototype.$digest = function () {
     var ttl = 10;
     var dirty;
     this.$$lastDirtyWatch = null;
+    this.$beginPhase('$digest');
 
     do {
+        while (this.$$asyncQueue.length) {
+            var asyncTask = this.$$asyncQueue.shift();
+            asyncTask.scope.$eval(asyncTask.expression);
+        }
         dirty = this.$$digestOnce();
-        if (dirty && !(ttl--)) {
+        if ((dirty || this.$$asyncQueue.length) && !(ttl--)) {
             throw '10 digest iterations reached';
         }
-    } while (dirty);
+    } while (dirty|| this.$$asyncQueue.length);
+    this.$clearPhase();
 };
 
 Scope.prototype.$$areEqual = function(newValue, oldValue, valueEq) {
@@ -90,10 +98,35 @@ Scope.prototype.$eval = function(expr, locals) {
 
 Scope.prototype.$apply = function(expr) {
     try {
+        this.$beginPhase('$apply');
         return this.$eval(expr);
     } finally {
+        this.$clearPhase();
         this.$digest();
     }
+};
+
+Scope.prototype.$evalAsync = function(expr) {
+    var self = this;
+    if (!self.$$phase && !self.$$asyncQueue.length) {
+        setTimeout(function() {
+            if (self.$$asyncQueue.length) {
+                self.$digest();
+            }
+        }, 0);
+    }
+    this.$$asyncQueue.push({scope: this, expression: expr});
+};
+
+Scope.prototype.$beginPhase = function(phase) {
+    if (this.$$phase) {
+        throw this.$$phase + ' already in progress.';
+    }
+    this.$$phase = phase;
+};
+
+Scope.prototype.$clearPhase = function() {
+    this.$$phase = null;
 };
 
 module.exports = Scope;
