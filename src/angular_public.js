29c28ecce46c631ed903dea9f752afc8c0463aef
Le Do Thai Binh <xeasony@gmail.com>
2016-12-11 11:19:23 +0100

    Chapter 14: implement $q.all, ES2015 compliance promise, $$q

--- src/angular_public.js
+++ src/angular_public.js
@@ -8,7 +8,8 @@ function publishExternalAPI() {
     ngModule.provider('$filter', require('./filter'));
     ngModule.provider('$parse', require('./parse'));
     ngModule.provider('$rootScope', require('./scope'));
-    ngModule.provider('$q', require('./q'));
+    ngModule.provider('$q', require('./q').$QProvider);
+    ngModule.provider('$$q', require('./q').$$QProvider);
 }
 
 module.exports = publishExternalAPI;
