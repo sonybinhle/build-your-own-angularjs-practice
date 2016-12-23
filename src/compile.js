55f14ff83fec3b10d615c2963265466d94bb8579
Le Do Thai Binh <xeasony@gmail.com>
2016-12-23 05:39:19 +0100

    Chapter 18: implement scope for directive when directive ask about it

--- src/compile.js
+++ src/compile.js
@@ -187,6 +187,10 @@ function $CompileProvider($provide) {
                     childLinkFn = compileNodes(node.childNodes);
                 }
 
+                if (nodeLinkFn && nodeLinkFn.scope) {
+                    attrs.$$element.addClass('ng-scope');
+                }
+
                 if (nodeLinkFn || childLinkFn) {
                     linkFns.push({
                         nodeLinkFn: nodeLinkFn,
@@ -204,16 +208,21 @@ function $CompileProvider($provide) {
                 });
 
                 _.forEach(linkFns, function(linkFn) {
+                    var node = stableNodeList[linkFn.idx];
                     if (linkFn.nodeLinkFn) {
+                        if (linkFn.nodeLinkFn.scope) {
+                            scope = scope.$new();
+                            $(node).data('$scope', scope);
+                        }
                         linkFn.nodeLinkFn(
                             linkFn.childLinkFn,
                             scope,
-                            stableNodeList[linkFn.idx]
+                            node
                         );
                     } else {
                         linkFn.childLinkFn(
                             scope,
-                            stableNodeList[linkFn.idx].childNodes
+                            node.childNodes
                         );
                     }
                 });
@@ -255,6 +264,7 @@ function $CompileProvider($provide) {
             var terminalPriority = -Number.MAX_VALUE;
             var terminal = false;
             var preLinkFns = [], postLinkFns = [];
+            var newScopeDirective;
 
             function addLinkFns(preLinkFn, postLinkFn, attrStart, attrEnd) {
                 if (preLinkFn) {
@@ -279,6 +289,11 @@ function $CompileProvider($provide) {
                 if (directive.priority < terminalPriority) {
                     return false;
                 }
+
+                if (directive.scope) {
+                    newScopeDirective = newScopeDirective || directive;
+                }
+
                 if (directive.compile) {
                     var linkFn = directive.compile($compileNode, attrs);
                     var attrStart = directive.$$start;
@@ -314,6 +329,7 @@ function $CompileProvider($provide) {
             }
 
             nodeLinkFn.terminal = terminal;
+            nodeLinkFn.scope = newScopeDirective && newScopeDirective.scope;
 
             return nodeLinkFn;
         }
